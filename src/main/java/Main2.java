7cad6a17acec1e1d56ca8c2bc7137ad05270e907
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-01-21 10:54:06 +0300

    Update Main2.java

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,15 +1,15 @@
 public class Main2 {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-
-        int number = 10;
-        for (int i = 0; i < 1_000_000_000; i++) {
-            number += i + i;
+        
+        String line = "";
+        for (int i = 0; i < 100_000; i++) {
+            line += Math.random();
         }
 
-        long end = System.currentTimeMillis() - start;
-        System.out.println("За " + end + " миллисекунд[ы] выполнилось 1 миллиарда операций");
+        long end = System.currentTimeMillis();
 
-
+        long difference = end - start;
+        System.out.println("Цикл for работал " + (difference / 1000) + " секунд!");
     }
 }
